9d5b05734be0804a72ee9e9722231fd9ff11855d
HNU <qkfkaroql79@naver.com>
2023-03-14 13:18:55 +0900

    초기 커밋.

---
+++ son/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace son
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.Write("Write"); Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+
+            //2
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+
+            //3
+            Console.Write("Write");
+            Console.WriteLine("WriteLine");
+
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+
+            Console.Write("Write");
+            Console.Write("Write");
+        }
+    }
+}
